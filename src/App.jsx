a4feb74d7661e78f78d1f3270a18316705e21879
Егор Асонов <egorasonovvv@gmail.com>
2024-09-14 23:35:14 +0600

    funny cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import { EffectCube } from 'swiper/modules';
+import 'swiper/scss';
+import 'swiper/css/effect-cube';
+
 function App() {
   return (
-      <div>hello</div>
-  )
+      <Swiper
+          effect={'cube'}
+          grabCursor={true}
+          cubeEffect={{
+              shadow: true,
+              slideShadows: true,
+              shadowOffset: 20,
+              shadowScale: 0.94,
+          }}
+          modules={[EffectCube]}
+
+      >
+          <SwiperSlide>
+              <img src='/forest.jpg' />
+          </SwiperSlide>
+          <SwiperSlide>
+              <img src='/bridge.jpg' />
+          </SwiperSlide>
+          <SwiperSlide>
+              <img src='/nastya.jpg' />
+          </SwiperSlide>
+          <SwiperSlide>
+              <img src='/cat.jpg' />
+          </SwiperSlide>
+      </Swiper>
+  );
 }
 
 export default App;
